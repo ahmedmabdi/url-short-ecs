f25c463582186e6fabc5fd40ad907dba6c22db0a
ahmedmabdi <ahmed.abdi67@outlook.com>
2026-03-12 17:40:54 +0000

    changes to github/workflows, app, adding codeploy/

---
+++ terraform/global/main.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "codedeploy_artifacts" {
+  bucket = "codedeploy-artifacts-ecs"
+
+  tags = {
+    Name        = "codedeploy-artifacts"
+    Environment = "shared"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.codedeploy_artifacts.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
