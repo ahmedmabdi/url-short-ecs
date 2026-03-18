7529f0defb6faa1cb7875f6506431d237a94df41
ahmedmabdi <ahmed.abdi67@outlook.com>
2026-03-18 00:28:33 +0000

    updating github workflow files- to work on pr

--- app/src/ddb.py
+++ app/src/ddb.py
@@ -5,8 +5,6 @@ _table = boto3.resource("dynamodb", region_name=os.environ["AWS_REGION"]).Table(
 def put_mapping(short_id: str, url: str):
     _table.put_item(Item={"id": short_id, "url": url})
 
-
-
 def get_mapping(short_id: str):
     resp = _table.get_item(Key={"id": short_id})
     return resp.get("Item")
