8264049301aba6c7c5c256a306a3c08a4bd57251
ahmedmabdi <ahmed.abdi67@outlook.com>
2026-04-19 09:43:25 +0100

    edit dockerfile and terrafrom/

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -11,17 +11,17 @@ variable "vpc_cidr" {
 
 variable "azs" {
   type    = list(string)
-  default = ["eu-west-2a", "eu-west-2b"]
+  default = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
 }
 
 variable "public_subnet_cidrs" {
   type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   type    = list(string)
-  default = ["10.0.101.0/24", "10.0.102.0/24"]
+  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 variable "private_subnet_ids" {
   type = list(string)
